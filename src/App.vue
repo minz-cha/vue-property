c374236a10215c17f1e0f5c36cf82c07eeb78ffc
minju kim <demin1026@kpu.ac.kr>
2023-03-12 16:55:27 +0900

    반복문

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+
+  <div class = "menu">
+
+    <!-- 반복문: 'v-for="작명 in 몇회" :key=""' 
+    반복문 쓸 때 ':key="작명"' 안쓰면 에러남 -->
+    <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
+    <!-- 'v-for="(a,i) in 메뉴들" :key="i"'
+    처럼 변수 작명 2개까지 가능 - 왼쪽변수: array내의 데이터, 오른쪽변수: 1씩 증가하는 함수 -->
+
+  </div>
+
   <div>
     <h4 :style="스타일">{{ products[0] }}</h4> 
     <p>{{ price1 }} 만원</p>
@@ -24,8 +34,9 @@ export default {
       price1 : 60,
       price2 : 70,
       price3 : 80,
+      메뉴들: ['Home', 'Shop', 'About'],
       products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
-      스타일: 'color : #00008B',
+      스타일: 'color : darkslateblue',
     }
   },
   components: {
@@ -40,6 +51,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
+}
+
 </style>
