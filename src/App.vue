640a29fb8b7a082768d807673085f54e87cb5401
minju kim <demin1026@kpu.ac.kr>
2023-04-17 19:52:14 +0900

    메서드 헨들러 이용하여 신고기능 생성

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
     <a v-for="a in menu" :key="a">{{ a }}</a>
   </div>
 
-  <div v-for="i in 3" :key="i">
+  <div v-for="(a,i) in 3" :key="i">
     <h4>{{ products[i] }}</h4>
     <p>{{ price[i] }}만원</p>
-    <button @click="신고수[i] += 1">허위매물 신고</button> <span>신고수: {{신고수[i]}} </span>
+    <button @click="increase(i)">허위매물 신고</button> <span>신고수: {{신고수[i]}} </span>
   </div>
 </template>
 
@@ -27,7 +27,13 @@ export default {
       스타일: 'color : darkslateblue',
     }
   },
-  
+  // vue에서 함수 생성
+  // methods : { 함수(){} }
+  methods : {
+    increase(id){
+      this.신고수[id] += 1;
+    }
+  },
   components: {
   }
 }
