5251ad5e165e606b9df24da4191844e34aa32d3a
minju kim <demin1026@kpu.ac.kr>
2023-04-21 19:28:16 +0900

    [ADD] 각 품목마다 상품 이미지 첨부

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   </div>
 
   <div v-for="(a,i) in 3" :key="i">
+    <img :src="require('./assets/room' + i + '.jpg')" class = "room-img">
     <h4>{{ products[i] }}</h4>
     <p>{{ price[i] }}만원</p>
     <button @click="increase(i)">허위매물 신고</button> <span>신고수: {{신고수[i]}} </span>
@@ -18,17 +19,12 @@ export default {
   data(){
     return {
       신고수 : [0, 0, 0], 
-      price1 : 60,
-      price2 : 70,
-      price3 : 80,
       menu: ['Home', 'Shop', 'About'],
       products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
       price : ['60', '70', '80'],
       스타일: 'color : darkslateblue',
     }
   },
-  // vue에서 함수 생성
-  // methods : { 함수(){} }
   methods : {
     increase(id){
       this.신고수[id] += 1;
@@ -40,6 +36,11 @@ export default {
 </script>
 
 <style>
+.room-img{
+  width: 100%;
+  margin-top:  40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
